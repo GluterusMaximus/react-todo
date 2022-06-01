92b30939abf24fe3e5e83bdd60e1ba5d9c67bad0
GluterusMaximus <supkar1756@gmail.com>
2022-06-01 22:14:24 +0300

    Initial express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-a = 5
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+console.log(process.env.PORT)
+
+app.listen(PORT, console.log('Server running'))
