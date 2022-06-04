cd80b95d6d317afae06fdeb1e89fbb38124f1a55
GluterusMaximus <supkar1756@gmail.com>
2022-06-04 21:51:31 +0300

    Added homescreen and router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <main>
+        <Routes>
+          <Route path='/' element={<HomeScreen/>}></Route>
+        </Routes>
+      </main>
+    </Router>
+  )
 }
 
 export default App
