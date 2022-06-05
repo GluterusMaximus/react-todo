9ef5499b3ce9e5d7aa8c021079d52aedded98f14
GluterusMaximus <supkar1756@gmail.com>
2022-06-05 12:25:02 +0300

    Added dotenv package

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT || 5000
-
-console.log(process.env.PORT)
 
 app.listen(PORT, console.log('Server running'))
