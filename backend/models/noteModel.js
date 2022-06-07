10786afec0a69d744b95f49a7e503ca16ffe80fe
GluterusMaximus <supkar1756@gmail.com>
2022-06-07 21:41:44 +0300

    Database models

---
+++ backend/models/noteModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const noteSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  { timestamps: true }
+)
+
+const Note = mongoose.model('User', noteSchema)
+
+export default Note
