58a270d50622fe3a46fb5b0585be60049f19fe32
GluterusMaximus <supkar1756@gmail.com>
2022-06-08 17:20:53 +0300

    User routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/connectDB.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDB()
 const app = express()
+
+app.use(express.json())
+
+app.use('/users', userRoutes)
+app.use('/', (req, res) => {
+  res.send('Api is running...')
+})
 
 const PORT = process.env.PORT || 5000
 
