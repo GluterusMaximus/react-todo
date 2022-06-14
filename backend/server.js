e66bdb70b965c16027bbe28c5592b520bc8c3b0e
GluterusMaximus <supkar1756@gmail.com>
2022-06-14 21:56:09 +0300

    Some addition express middleware

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/connectDB.js'
 import userRoutes from './routes/userRoutes.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 app.use('/users', userRoutes)
 app.use('/', (req, res) => {
