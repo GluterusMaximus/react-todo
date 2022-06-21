ded9d725f453a0382b466b76f16bd47a9067ba53
GluterusMaximus <supkar1756@gmail.com>
2022-06-21 22:19:09 +0300

    User registration

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -2,6 +2,8 @@
 import User from '../models/userModel.js'
 import uuid from 'uuid'
 import mailService from './mailService.js'
+import tokenService from './tokenService.js'
+import UserDto from '../dtos/userDto.js'
 
 class UserService {
   async register(email, password) {
@@ -10,10 +12,16 @@ class UserService {
     if (userExists) {
       throw new Error('User already exists')
     }
-
     const activationLink = uuid.v4()
+
     const user = await User.create({ email, password, activationLink })
     await mailService.sendActivationMail(email, activationLink)
+
+    const userDto = new UserDto(user)
+    const tokens = tokenService.generateTokens({ ...userDto })
+    await tokenService.saveToken(userDto.id, tokens.refreshToken)
+
+    return { ...tokens, user: userDto }
   }
 }
 
